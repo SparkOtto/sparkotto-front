512d3f517f14c9ba00129d6277475b8d7b7a7908
MathieuLP35 <skayagta@gmail.com>
2025-05-20 10:43:22 +0200

    feature-Lot4-29-Front-VoirEtModifierSonProfil : Refactor Sidebar component to conditionally render admin and user buttons based on the current route

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,12 +6,15 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from './Logo';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Sidebar() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const router = useRouter();
 
   const SidebarContent = () => (
     <>
@@ -21,15 +24,28 @@ export default function Sidebar() {
         </Link>
       </div>
       <Nav className="flex-column">
-        <Button variant="yellow" className="mb-3 text-dark">
-          Réserver un Véhicule
-        </Button>
-        <Button variant="yellow" className="mb-3 text-dark">
-          Ajouter un véhicule
-        </Button>
-        <Button variant="yellow" className="mb-3 text-dark">
-          Mes trajets à venir
-        </Button>
+        {router.pathname.startsWith('/admin') ? (
+          <>
+            <Button variant="yellow" className="mb-3 text-dark" onClick={() => router.push('/admin/user')}>
+              Gérer les utilisateurs
+            </Button>
+            <Button variant="yellow" className="mb-3 text-dark" onClick={() => router.push('/admin/car')}>
+              Gérer les véhicules
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button variant="yellow" className="mb-3 text-dark">
+              Réserver un Véhicule
+            </Button>
+            <Button variant="yellow" className="mb-3 text-dark">
+              Ajouter un véhicule
+            </Button>
+            <Button variant="yellow" className="mb-3 text-dark">
+              Mes trajets à venir
+            </Button>
+          </>
+        )}
       </Nav>
       <div className="mt-auto text-center">
         <Button variant="light" className="d-flex align-items-center">
